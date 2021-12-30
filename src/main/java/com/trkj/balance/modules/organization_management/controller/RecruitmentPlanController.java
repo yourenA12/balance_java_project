312d76f4445bf3dd2607ea687aa9f55a00779f80
刘耀华 <1489086370@qq.com>
2021-12-30 20:28:40 +0800

    zhaopin

--- src/main/java/com/trkj/balance/modules/organization_management/controller/RecruitmentPlanController.java
+++ src/main/java/com/trkj/balance/modules/organization_management/controller/RecruitmentPlanController.java
@@ -1,4 +1,4 @@
-package com.trkj.balance.modules.employee_management.controller;
+package com.trkj.balance.modules.organization_management.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
  * 招聘计划表 前端控制器
  * </p>
  *
- * @author 林落
- * @since 2021-12-29
+ * @author 友人A
+ * @since 2021-12-30
  */
 @RestController
 @RequestMapping("/recruitmentPlan")
-public class  RecruitmentPlanController {
+public class RecruitmentPlanController {
 
 }
 
