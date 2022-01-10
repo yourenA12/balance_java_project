b5a97d3cd6e5587c07e923ee8fe7f3fbd76e6f8c
syj <2398646027@qq.com>
2022-01-10 16:42:16 +0800

    审批主表的单表分页查询

---
+++ src/main/java/com/trkj/balance/modules/examine_management/controller/WorkerController.java
@@ -0,0 +1,21 @@
+package com.trkj.balance.modules.examine_management.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 转正 前端控制器
+ * </p>
+ *
+ * @author jiejie
+ * @since 2022-01-04
+ */
+@RestController
+@RequestMapping("/worker")
+public class WorkerController {
+
+}
+
---
+++ src/main/java/com/trkj/balance/modules/examine_management/service/WorkerService.java
@@ -0,0 +1,16 @@
+package com.trkj.balance.modules.examine_management.service;
+
+import com.trkj.balance.modules.examine_management.entity.Worker;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 转正 服务类
+ * </p>
+ *
+ * @author jiejie
+ * @since 2022-01-04
+ */
+public interface WorkerService extends IService<Worker> {
+
+}
