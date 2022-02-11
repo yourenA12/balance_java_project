c90a68df10e1e58c324682564a92b59bd0a5120a
达鸭 <2212511473@qq.com>
2022-02-11 22:39:16 +0800

    wjh

--- src/main/java/com/trkj/balance/modules/organization_management/service/impl/DeptServiceImpl.java
+++ src/main/java/com/trkj/balance/modules/organization_management/service/impl/DeptServiceImpl.java
@@ -4,8 +4,12 @@ import com.trkj.balance.modules.organization_management.entity.Dept;
 import com.trkj.balance.modules.organization_management.mapper.DeptMapper;
 import com.trkj.balance.modules.organization_management.service.DeptService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.trkj.balance.modules.system_managementTest.entity.Deptp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -36,5 +40,45 @@ public class DeptServiceImpl extends ServiceImpl<DeptMapper, Dept> implements De
     @Override
     public int amend(Dept dept) {
         return deptMapper.updateById(dept);
+    }
+
+
+    @Override
+    public List<Dept> queryList() {
+        List<Dept> data = deptMapper.selectList(null);
+        List<Dept> deptList = new ArrayList<>();
+        for (Dept dept : data){
+            if (dept.getDeptId()+""!="" && dept.getDeptId()!=null && dept.getDeptId() ==1){
+                deptList.add(dept);
+            }
+        }
+        for (Dept dept : deptList){
+            dept.setChildren(getChilde(dept.getDeptId(),data));
+        }
+        return deptList;
+    }
+
+
+    private List<Dept> getChilde(Long id, List<Dept> rootList){
+        //子级
+        List<Dept> childList = new ArrayList<>();
+        for(Dept dept : rootList){
+            // 遍历所有节点，将父级id与传过来的id比较
+            if(dept.getDeptPid()+""!="" && dept.getDeptPid()!=null && dept.getDeptPid().equals(id)){
+                childList.add(dept);
+            }
+        }
+        // 把子级的子级再循环一遍
+        for(Dept dept : childList){
+            if(dept.getDeptPid()+""!="" && dept.getDeptPid()!=null ){
+                dept.setChildren(getChilde(dept.getDeptId(), rootList));
+            }
+
+        }
+        // 递归退出条件
+        if (childList.size() == 0){
+            return null;
+        }
+        return childList;
     }
 }
