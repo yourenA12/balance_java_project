53f90c8e447131896436b5419000262daec48979
yourenA12 <3092079817@qq.com>
2022-02-24 14:40:15 +0800

    友人A

--- src/main/java/com/trkj/balance/modules/social_management/service/InsuredDeptPostService.java
+++ src/main/java/com/trkj/balance/modules/social_management/service/InsuredDeptPostService.java
@@ -3,6 +3,8 @@ package com.trkj.balance.modules.social_management.service;
 import com.trkj.balance.modules.social_management.entity.InsuredDeptPost;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.ArrayList;
+
 /**
  * <p>
  * 参保方案部门职位中间表 服务类
@@ -12,5 +14,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-02-22
  */
 public interface InsuredDeptPostService extends IService<InsuredDeptPost> {
+
+    // 根据部门id和职位id，查询参保方案部门职位中间表有无数据，
+    String selectInsuredDeptPost(ArrayList<Integer> deptIds, ArrayList<Integer> postIds);
 
 }
