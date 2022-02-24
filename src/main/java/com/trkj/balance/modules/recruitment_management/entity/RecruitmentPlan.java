6d47ff98458eabfb3ab2d86d6cf0c73d5e0112bd
刘耀华 <1489086370@qq.com>
2022-02-24 14:32:47 +0800

    zhaopin

--- src/main/java/com/trkj/balance/modules/recruitment_management/entity/RecruitmentPlan.java
+++ src/main/java/com/trkj/balance/modules/recruitment_management/entity/RecruitmentPlan.java
@@ -1,9 +1,8 @@
 package com.trkj.balance.modules.recruitment_management.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -23,6 +22,7 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("RECRUITMENT_PLAN")
+@KeySequence(value = "RECRUITMENT_PLAN_ID",clazz = Long.class)
 @ApiModel(value="RecruitmentPlan对象", description="招聘计划表")
 public class RecruitmentPlan implements Serializable {
 
@@ -73,17 +73,19 @@ public class RecruitmentPlan implements Serializable {
     private Long staffId;
 
     @ApiModelProperty(value = "创建时间")
-    @TableField("CREATED_TIME")
+    @TableField(value = "CREATED_TIME",fill = FieldFill.INSERT)
     private Date createdTime;
 
     @ApiModelProperty(value = "修改时间")
-    @TableField("UPDATED_TIME")
+    @TableField(value = "UPDATED_TIME",fill = FieldFill.INSERT_UPDATE)
     private Date updatedTime;
 
+    @Version
     @ApiModelProperty(value = "乐观锁")
     @TableField("REVISION")
     private Long revision;
 
+    @TableLogic
     @ApiModelProperty(value = "逻辑删除;1表示删除，0 表示未删除")
     @TableField("IS_DELETED")
     private Long isDeleted;
--- src/main/java/com/trkj/balance/modules/recruitment_management/service/RecruitmentPlanVoService.java
+++ src/main/java/com/trkj/balance/modules/recruitment_management/service/RecruitmentPlanVoService.java
@@ -4,10 +4,13 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.trkj.balance.modules.recruitment_management.vo.RecruitmentPlanVo;
+import com.trkj.balance.modules.recruitment_management.vo.ResumeVo;
 
 public interface RecruitmentPlanVoService extends IService<RecruitmentPlanVo> {
 
-    IPage<RecruitmentPlanVo> selectPage(Page<RecruitmentPlanVo> page);
+
+
+    IPage<RecruitmentPlanVo> selectPage(int page, int size, String recruitmentPlanName);
 
 
 }
