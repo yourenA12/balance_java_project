943f2cef5ae498304d7f111d18e1999c11484b59
chendou <1427760246@qq.com>
2022-03-01 18:51:29 +0800

    林落。

--- src/main/java/com/trkj/balance/modules/social_management/service/impl/InsuredDetailSonServiceImpl.java
+++ src/main/java/com/trkj/balance/modules/social_management/service/impl/InsuredDetailSonServiceImpl.java
@@ -1,10 +1,14 @@
 package com.trkj.balance.modules.social_management.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.trkj.balance.modules.social_management.entity.InsuredDetailSon;
 import com.trkj.balance.modules.social_management.mapper.InsuredDetailSonMapper;
 import com.trkj.balance.modules.social_management.service.InsuredDetailSonService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class InsuredDetailSonServiceImpl extends ServiceImpl<InsuredDetailSonMapper, InsuredDetailSon> implements InsuredDetailSonService {
 
+    @Autowired
+    private InsuredDetailSonMapper insuredDetailSonMapper;
+
+    @Override
+    public List<InsuredDetailSon> selectInsuredDetailSon(Long id) {
+        QueryWrapper wrapper=new QueryWrapper<>();
+        wrapper.eq("STAFF_ID",id);
+        return insuredDetailSonMapper.selectList(wrapper);
+    }
 }
