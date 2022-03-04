575060800518aae8ea9f3d7eb656e0af72135d94
MsShi <2398646027@qq.com>
2022-03-04 10:18:24 +0800

    更新

--- src/test/java/com/trkj/balance/DateTest.java
+++ src/test/java/com/trkj/balance/DateTest.java
@@ -56,4 +56,51 @@ public class DateTest {
         System.out.println(dayComparePrecise("2021-12-12","2023-09-18"));
     }
 
+    @Test
+    public void c() throws ParseException {
+        // 补上午
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+        //使用默认时区和语言环境获得一个日历。
+        Calendar    rightNow    =    Calendar.getInstance();
+        /*用Calendar的get(int field)方法返回给定日历字段的值。
+        HOUR 用于 12 小时制时钟 (0 - 11)，HOUR_OF_DAY 用于 24 小时制时钟。*/
+        Integer year = rightNow.get(Calendar.YEAR);
+        Integer month = rightNow.get(Calendar.MONTH)+1; //第一个月从0开始，所以得到月份＋1
+        Integer day = rightNow.get(rightNow.DAY_OF_MONTH);
+        Integer hour24 = rightNow.get(rightNow.HOUR_OF_DAY);
+        Integer minute = rightNow.get(rightNow.MINUTE);
+        Integer second = rightNow.get(rightNow.SECOND);
+        Integer millisecond = rightNow.get(rightNow.MILLISECOND);
+        String TimeNow24 = year+"-"+month+"-"+day+" "+"08:00:00:000";
+
+        Date date=sdf.parse(TimeNow24);
+
+
+        System.out.println(date);
+
+        System.out.println(TimeNow24);
+
+
+    }
+
+    @Test
+    public void getTimeNowThroughCalendar(){
+        //使用默认时区和语言环境获得一个日历。
+        Calendar    rightNow    =    Calendar.getInstance();
+        /*用Calendar的get(int field)方法返回给定日历字段的值。
+        HOUR 用于 12 小时制时钟 (0 - 11)，HOUR_OF_DAY 用于 24 小时制时钟。*/
+        Integer year = rightNow.get(Calendar.YEAR);
+        Integer month = rightNow.get(Calendar.MONTH)+1; //第一个月从0开始，所以得到月份＋1
+        Integer day = rightNow.get(rightNow.DAY_OF_MONTH);
+        Integer hour12 = rightNow.get(rightNow.HOUR);
+        Integer hour24 = rightNow.get(rightNow.HOUR_OF_DAY);
+        Integer minute = rightNow.get(rightNow.MINUTE);
+        Integer second = rightNow.get(rightNow.SECOND);
+        Integer millisecond = rightNow.get(rightNow.MILLISECOND);
+        String TimeNow12 = year+"-"+month+"-"+day+" "+hour12+":"+minute+":"+second+":"+millisecond;
+        String TimeNow24 = year+"-"+month+"-"+day+" "+hour24+":"+minute+":"+second+":"+millisecond;
+        System.out.println("日历："+rightNow+"\n12小时制时钟："+TimeNow12+"\n24小时制时钟："+TimeNow24);
+    }
+
 }
